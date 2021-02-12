24e3ce44d4e431a69b1ee74d7452a68d1aedcc61
madiej6 <madelinejones214@gmail.com>
2021-02-12 10:45:33 -0500

    get_parcels working for states without ORNL building centroids

--- WorkingScripts/main.py
+++ WorkingScripts/main.py
@@ -2,6 +2,7 @@ import os
         
 import o1_Earthquake_ShakeMap_Download
 import o2_Earthquake_ShakeMap_Into_CensusGeographies
+import o3_Earthquake_GetParcels
 
 def main(testingmode = False):
     if not testingmode:
@@ -14,15 +15,19 @@ def main(testingmode = False):
     else:
         # if testing mode, use the napa 2014 shakemap
         print('testing mode')
-        new_events = [r"C:\Projects\FEMA\EarthquakeModel\ShakeMaps\napa2014shakemap_fortesting"]
+        #new_events = [r"C:\Projects\FEMA\EarthquakeModel\ShakeMaps\napa2014shakemap_fortesting"]
+        new_events = [r"C:\Projects\FEMA\EarthquakeModel\ShakeMaps\idaho2017shakemap_fortesting"]
 
     for event in new_events:
         print('\nCensus Data Processing for: ', event)
-        o2_Earthquake_ShakeMap_Into_CensusGeographies.shakemap_into_census_geo(event)
+        o2_Earthquake_ShakeMap_Into_CensusGeographies.shakemap_into_census_geo(eventdir = event)
+
+        print('\nGathering Building Outlines for: ', event)
+        o3_Earthquake_GetParcels.shakemap_get_bldgs(eventdir = event)
 
     return
 
 
 if __name__ == "__main__":
-    main(testingmode=False)
+    main(testingmode=True)
 
