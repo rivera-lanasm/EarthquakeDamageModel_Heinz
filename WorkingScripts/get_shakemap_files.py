2ad87fa0920a35eba586dc99f7b2bf85a6ef45bb
madiej6 <madelinejones214@gmail.com>
2021-03-15 12:57:54 -0400

    .gitignore is now working

--- WorkingScripts/get_shakemap_files.py
+++
@@ -1,6 +0,0 @@
-
-def get_shakemap_files(ShakeMapDir):
-    mi = "{}\mi.shp".format(ShakeMapDir)
-    pgv = "{}\pgv.shp".format(ShakeMapDir)
-    pga = "{}\pga.shp".format(ShakeMapDir)
-    return mi, pgv, pga
