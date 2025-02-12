88b525a1d07a226e99e40308839f5b89982d16e5
ItsMeriem <149505081+ItsMeriem@users.noreply.github.com>
2025-02-12 14:50:27 -0500

    Update get_shakemap_files.py
    Added docstrings for function

--- WorkingScripts/get_shakemap_files.py
+++ WorkingScripts/get_shakemap_files.py
@@ -1,5 +1,15 @@
 
 def get_shakemap_files(ShakeMapDir):
+    """
+    Generate file paths for ShakeMap shapefiles in the given directory.
+
+    Args:
+        ShakeMapDir (str): Path to the ShakeMap directory. This gets defined and stored in get_file_paths.py
+
+    Returns:
+        tuple: Paths to 'mi.shp', 'pgv.shp', and 'pga.shp' files.
+    """
+    
     mi = "{}\mi.shp".format(ShakeMapDir)
     pgv = "{}\pgv.shp".format(ShakeMapDir)
     pga = "{}\pga.shp".format(ShakeMapDir)
