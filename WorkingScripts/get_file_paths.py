5c6f84041da729c2d115d4752e28b72e1f234fd2
ItsMeriem <149505081+ItsMeriem@users.noreply.github.com>
2025-02-12 14:26:51 -0500

    Update get_file_paths.py
    Added comments

--- WorkingScripts/get_file_paths.py
+++ WorkingScripts/get_file_paths.py
@@ -2,8 +2,13 @@ import os
 
 def get_shakemap_dir():
     # Set file path to save ShakeMap zip files to
+
+    # Check if a directory named "ShakeMaps" exists in the parent directory of the current working directory.
+    # If the "ShakeMaps" directory exists, assign its path to the variable ShakeMapDir.
     if os.path.exists(os.path.join(os.path.dirname(os.getcwd()), 'ShakeMaps')):
         ShakeMapDir = os.path.join(os.path.dirname(os.getcwd()), 'ShakeMaps')
+
+    # If the "ShakeMaps" directory does not exist, create the directory and assign the path to the variable ShakeMapDir
     else:
         os.mkdir(os.path.join(os.path.dirname(os.getcwd()), 'ShakeMaps'))
         ShakeMapDir = os.path.join(os.path.dirname(os.getcwd()), 'ShakeMaps')
