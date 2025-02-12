8f36765ba621c038afb9765b11801e38ae7697db
ItsMeriem <149505081+ItsMeriem@users.noreply.github.com>
2025-02-12 14:53:05 -0500

    Update get_shakemap_files.py
    Comments

--- WorkingScripts/get_shakemap_files.py
+++ WorkingScripts/get_shakemap_files.py
@@ -4,7 +4,7 @@ def get_shakemap_files(ShakeMapDir):
     Generate file paths for ShakeMap shapefiles in the given directory.
 
     Args:
-        ShakeMapDir (str): Path to the ShakeMap directory. This gets defined and stored in get_file_paths.py
+        ShakeMapDir (str): Path to the ShakeMap directory. The ShakeMapDir variable gets defined and stored in get_file_paths.py
 
     Returns:
         tuple: Paths to 'mi.shp', 'pgv.shp', and 'pga.shp' files.
